ff0ed2181dcb320c713f986ea7fc6c6a41d02b6e
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-04-09 11:52:51 +0100

    Slight modification to the 'string-validators' program.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- Projects/Project 0/Strings/string-validators.py
+++ Projects/Project 0/Strings/string-validators.py
@@ -1,15 +1,51 @@
 #!/usr/bin/env  python3
 
+"""
+    Program purpose: Validates characters in the string.
+    Program Author : Happi Yvan
+    Author email   : ivensteinpoker@gmail.com
+"""
 
 def process(_str):
-        print(any(c.isalnum() for c in _str))
-        print(any(c.isalpha() for c in _str))
-        print(any(c.isdigit() for c in _str))
-        print(any(c.islower() for c in _str))
-        print(any(c.isupper() for c in _str))
+
+        print("\n\tIs there any alphanumeric character: ", end="")
+        if any(c.isalnum() for c in _str):
+            print("True")
+        else:
+            print("False")
+
+        print("\tIs there any alphabetic character: ", end="")
+        if any(c.isalpha() for c in _str):
+            print("True")
+        else:
+            print("False")
+
+        print("\tIs there any digit: ", end="")
+        if any(c.isdigit() for c in _str):
+            print("True")
+        else:
+            print("False")
+
+        print("\tIs there is any lower character: ", end="")
+        if any(c.islower() for c in _str):
+            print("True")
+        else:
+            print("False")
+
+        print("\tIs there any upper character: ", end="")
+        if any(c.isupper() for c in _str):
+            print("True")
+        else:
+            print("False")
+
+        # print(any(c.isalnum() for c in _str))
+        # print(any(c.isalpha() for c in _str))
+        # print(any(c.isdigit() for c in _str))
+        # print(any(c.islower() for c in _str))
+        # print(any(c.isupper() for c in _str))
 
 def main():
-    user_str = input("Enter a string: ")
+    user_str = input("\n\tEnter a string: ")
     process(user_str)
 
 if __name__ == "__main__":
