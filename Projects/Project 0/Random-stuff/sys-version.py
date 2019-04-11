1be4d9a040fc9ec1b32fef7eb8c6a56c48abd3b6
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-04-11 21:20:32 +0100

    Random programs 'sys-version' and 'twinkle-little-star' created!
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Project 0/Random-stuff/sys-version.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env  python3
+
+#: Program Purpose: Prints to the console the current python
+#:                  version installed
+#:
+#: Program Author : Happi Yvan <ivensteinpoker@gmail.com>
+#: Program Date   : 11/04/2019 (mm/dd/yyyy)
+
+import sys
+
+def main():
+    print(sys.version)
+
+if __name__ == '__main__':
+    main()
---
+++ Projects/Project 0/Random-stuff/twinkle-little-star.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env  python3
+
+#: Program Purpose: Program prints the poem 'Twinkle little' in a unique
+#:                  Fashion
+#:
+#: Program Author : Happi Yvan <ivenstienpoker@gmail.com>
+#: Program Date   : 11/04/2019 (mm/dd/yyyy)
+
+
+def print_poem():
+    print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!"
+          "\n\t\tUp above the world so high,\n\t\tLike the diamond in the sky\n"
+          "Twinkle, twinkle, little star,\n\tHow I wonder what you are")
+
+def main():
+    print_poem()
+
+if __name__ == '__main__':
+    main()
