938334f6c3de4d3b7139fd42a1812a560731ab7f
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-04-19 15:30:14 +0100

    Documentation added for program.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- Projects/Online Workouts/w3resource/Basic - Part-I/display-cal-for-month-and-year.py
+++ Projects/Online Workouts/w3resource/Basic - Part-I/display-cal-for-month-and-year.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+###################################################################################
+#                                                                                 #
+#       Program purpose: Display calendar for a specified day and year            #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                    #
+#       Creation Date  : 19/04/2019                                               #
+#                                                                                 #
+###################################################################################
+
 def display_calendar(year, month):
     import calendar
     print(calendar.month(year, month))
