6e528c05a4f7a8b02fbdd58afefa2af2fa613e5c
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-07-17 20:32:38 +0100

    Program concatenates the content of a list as a string and display results.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-27.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env  python3
+
+#############################################################################
+#                                                                           #
+#       Program purpose: Concatenates content of list as string and return  #
+#                        the results.                                       #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>              #
+#       Creation Date  : July 14, 2019                                      #
+#                                                                           #
+#############################################################################
+
+# URL: https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+
+def concat_list(some_list):
+    val = ''
+    for x in some_list:
+        val += str(x)
+    return val
+
+
+if __name__ == "__main__":
+    aList = [123, 'val', 'demo', 'test', 123.23]
+    print(f"Main list: {aList}\nConcatenated list as string: {concat_list(aList)}")
