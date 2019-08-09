0fcd3012ab1dc7cfeac9acefb787ac26c7d73de1
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-08-09 18:01:38 +0100

    Python program to run external command.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-45.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env  python3
+
+################################################################################
+#                                                                              #
+#       Program purpose: Program to call external command.                     #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                 #
+#       Creation Date  : August 9, 2019                                        #
+#                                                                              #
+################################################################################
+
+
+if __name__ == "__main__":
+    import os
+
+    os.system("whoami")  # displays name of user running this program
+    os.system("ls -la")  # Displays current system contents.
