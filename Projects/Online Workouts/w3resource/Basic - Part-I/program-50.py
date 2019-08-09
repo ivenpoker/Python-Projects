6aaffda713ea78e11dcd86499cdc00f82093ebca
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-08-09 18:21:38 +0100

    Program to print without newline or space
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-50.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env  python3
+
+###################################################################################
+#                                                                                 #
+#       Program purpose: Print without newline or space.                          #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                    #
+#       Creation Date  : August 9, 2019                                           #
+#                                                                                 #
+###################################################################################
+
+if __name__ == "__main__":
+    for i in range(0, 10):
+        print('*', end="")
+    print("\n")
