3cbf64c46d4a9a71445b9e06581a1ca3f0026e7a
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-08-15 15:31:48 +0100

    Yet another fix.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- Projects/Online Workouts/w3resource/Basic - Part-I/program-65.py
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-65.py
@@ -2,7 +2,7 @@
 
 #######################################################################################
 #                                                                                     #
-#       Program purpose: Convert seconds to day, hour, minutes and seconds               #
+#       Program purpose: Convert seconds to day, hour, minutes and seconds            #
 #       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
 #       Creation Date  : August 15, 2019                                              #
 #                                                                                     #
