062b492b335513b41f9fc05c6fadb7d3aceba450
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-08-19 12:45:38 +0100

    Get the copyright information for python.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-75.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Get the copyright information for python.                    #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : August 19, 2019                                              #
+#                                                                                     #
+#######################################################################################
+
+import sys
+
+if __name__ == "__main__":
+    print(f"\nPython Copyright Information: ")
+    print(sys.copyright)
