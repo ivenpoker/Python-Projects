fb86c7e3de16aa8aab20c15ab877d53e8e15d067
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-08-27 12:46:43 +0100

    Program to test if system is a little-endian or big-endian platform.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-77.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Test whether system is big-endian platform or little-endian  #
+#                        platform.                                                    #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : August 19, 2019                                              #
+#                                                                                     #
+#######################################################################################
+
+import sys
+
+if __name__ == "__main__":
+    if sys.byteorder == "little":
+        # intel, alpha
+        print("Little-endian platform.")
+    else:
+        # motorola, sparc
+        print("Big-endian platform.")
+
+    print()
