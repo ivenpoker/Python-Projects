4fb3395133111b06e08a6b670f0d53fb94c89dcf
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-08-29 16:04:59 +0100

    Program to define a string containing special characters in various forms.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-92.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Define a string containing special characters in             #
+#                        various forms.                                               #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : August 29, 2019                                              #
+#                                                                                     #
+#######################################################################################
+
+if __name__ == "__main__":
+    print()
+    print("\#{'}${\"}@/")
+    print("\#{'}${"'"'"}@/")
+    print(r"""\#{'}${"}@/""")
+    print('\#{\'}${"}@/')
+    print('\#{'"'"'}${"}@/')
+    print(r'''\#{'}${"}@/''')
+    print()
