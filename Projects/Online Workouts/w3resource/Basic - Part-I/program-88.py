791bb44ec2152a95d89b9c4a18ce5dc359c15e0b
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-08-29 15:01:58 +0100

    Program to perform addition and show sample operation.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-88.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Perform addition and show sample operation.                  #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : August 29, 2019                                              #
+#                                                                                     #
+#######################################################################################
+
+if __name__ == "__main__":
+    xVal = int(input("Enter value for x: "))
+    yVal = int(input("Enter value for y: "))
+
+    print(f"{xVal} + {yVal} = {xVal + yVal}")
+
+
