9fcf2ab1f431446b27a075bb45e28715153af194
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-08-30 22:22:33 +0100

    Program to get the identity of an object.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-93.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Program to get the identity of an object.                    #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : August 30, 2019                                              #
+#                                                                                     #
+#######################################################################################
+
+
+if __name__ == "__main__":
+    obj1 = object()
+    print(f"Object's id: {id(obj1)}")
