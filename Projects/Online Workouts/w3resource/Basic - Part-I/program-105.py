4cf5c116697a97daa768a9c647befa3e65ff1f96
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-08-31 00:12:42 +0100

    Program to get the user's environment variable.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-105.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Get the user's environment variables.                        #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : August 30, 2019                                              #
+#                                                                                     #
+#######################################################################################
+
+import os
+
+if __name__ == "__main__":
+
+    print(f"Environment: {os.environ}")
