e97ce52f53f7c3af4219a27f79e53cba58dcf6a1
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-08-30 22:30:42 +0100

    Program to get the current system time.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-98.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Get the current system time.                                 #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : August 30, 2019                                              #
+#                                                                                     #
+#######################################################################################
+
+import time
+
+if __name__ == "__main__":
+    print(f"Current system time: {time.ctime()}")
