343753930d67f87866b92844efd1b3b2f694cf65
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-01 02:28:45 +0100

    Program to print unicode characters.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-116.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Print unicode characters.                                    #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : September 1, 2019                                            #
+#                                                                                     #
+#######################################################################################
+
+if __name__ == "__main__":
+    tmpStr = u'\u0050\u0079\u0074\u0068\u006f\u006e \u0045\u0078\u0065\u0072\u0063\u0069\u0073\u0065\u0073 \u002d ' \
+          u'\u0077\u0033\u0072\u0065\u0073\u006f\u0075\u0072\u0063\u0065 '
+    print()
+    print(tmpStr)
+    print()
