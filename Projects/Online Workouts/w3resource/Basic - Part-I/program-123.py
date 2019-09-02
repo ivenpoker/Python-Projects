494a7bc2ebbb0007fc447213259ea1c8929f78ea
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-02 12:36:20 +0100

    Program to determine the largest and smallest integers, longs, floats.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-123.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Determine the largest and smallest integers, longs, floats.  #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : September 2, 2019                                            #
+#                                                                                     #
+#######################################################################################
+
+if __name__ == "__main__":
+    import sys
+    print(f"Float value information: {sys.float_info}")
+    print(f"\nInteger value information: {sys.int_info}")
+    print(f"\nMaximum size of an integer: {sys.maxsize}")
