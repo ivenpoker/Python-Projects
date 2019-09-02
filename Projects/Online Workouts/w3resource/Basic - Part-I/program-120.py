caf7005ecddb4cb68b3ff4579d397ae35df0e502
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-02 12:29:33 +0100

    Program to display a string with specific number of characters.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-120.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Displays a string with specific number of characters.        #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : September 1, 2019                                            #
+#                                                                                     #
+#######################################################################################
+
+if __name__ == "__main__":
+    stringNumber = "1234567890"
+    print("String of length 6: {:.6s}".format(stringNumber))
