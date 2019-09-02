2cfbf8824c867d0d220322d56c737d117542adf9
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-02 12:29:06 +0100

    Display a floating number in specified numbers.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-119.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Display a floating number in specified numbers.              #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : September 1, 2019                                            #
+#                                                                                     #
+#######################################################################################
+
+if __name__ == "__main__":
+    sampleFloat = 212.374767
+    print("\nMain float value is: {}".format(sampleFloat))
+    print("\nTo specified number (2) is: {:.2f}".format(sampleFloat))
+
