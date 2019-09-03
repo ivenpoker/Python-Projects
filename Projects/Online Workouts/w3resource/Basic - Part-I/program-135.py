560b93cd3ac366390d8c030bbd0e79d28dce7488
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-03 12:29:34 +0100

    Program to print a variable without spaces between values.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-135.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Print a variable without spaces between values.                  #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : September 2, 2019                                            #
+#                                                                                     #
+#######################################################################################
+
+
+if __name__ == "__main__":
+    x = 30
+    print(f"Value of x is {x}")
