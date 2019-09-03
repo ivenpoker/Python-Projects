a0ef70b80d75f4c1d70e23f25a7cdfe81bb9e0fe
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-03 12:44:41 +0100

    Program to convert an integer to binary keeping leading zeros.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-I/program-140.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Converts an integer to binary, keeping leading zeros.        #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : September 3, 2019                                            #
+#                                                                                     #
+#######################################################################################
+
+if __name__ == '__main__':
+    x = 12
+    print(format(x, '08b'))
+    print(format(x, '010b'))
