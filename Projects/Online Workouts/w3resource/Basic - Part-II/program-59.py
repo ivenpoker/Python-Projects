86d63a6490bc23e1424a1df210deb3d47e4016fc
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-10-10 13:03:08 +0100

    Program to compute the area of a polygon
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Basic - Part-II/program-59.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env  python3
+
+######################################################################################
+#                                                                                    #
+#       Program purpose: Computes the area of the polygon.                           #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                       #
+#       Creation Date  : October 10, 2019                                          #
+#                                                                                    #
+######################################################################################
+
+def poly_area(c):
+    add = []
+    for i in range(0, (len(c)-2), 2):
+        add.append(c[i] * c[i + 3] - c[i + 1] * c[i + 2])
+        add.append(c[len(c) - 2] * c[i] - c[len(c) - 1] * c[0]);
+    return abs(sum(add) / 2)
+
+if __name__ == "__main__":
+    print(poly_area([1, 0, 0, 0, 1, 1, 3, 0, -1, 1]))
