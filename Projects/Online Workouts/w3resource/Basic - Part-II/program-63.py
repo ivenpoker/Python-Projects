17ae8d9798199667b355c5fa36bb5e6e180817f6
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-10-10 16:20:46 +0100

    Added some bash configurations to program.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- Projects/Online Workouts/w3resource/Basic - Part-II/program-63.py
+++ Projects/Online Workouts/w3resource/Basic - Part-II/program-63.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env  python3
+
 ######################################################################################
 #                                                                                    #
 #       Program purpose: Adds up columns and rows of given table (matrix).           #
