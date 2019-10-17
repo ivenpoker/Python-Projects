37ab8b51b94abe3fc71455e3191430c7e30cdbbd
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-10-17 10:37:57 +0100

    Program to print a user given floating point number to 2 decimal places.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/String/program-30.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env  python3
+
+#######################################################################################
+#                                                                                     #
+#       Program purpose: Print a floating point number to 2 decimal places.           #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                        #
+#       Creation Date  : October 17, 2019                                             #
+#                                                                                     #
+#######################################################################################
+
+def get_user_data(input_mess: str):
+    is_valid = False
+    user_data = float(0)
+    while is_valid is False:
+        try:
+            user_data = float(input(input_mess))
+            is_valid = True
+        except RuntimeError as re:
+            print(f'[ERROR]: {re}')
+    return user_data
+
+if __name__ == "__main__":
+    user_float = get_user_data(input_mess='Enter some floating point number: ')
+    print(f"Float number '{user_float}' to 2 decimal places: {user_float:.2f}\n")
