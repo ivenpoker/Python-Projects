b752494103eda8c62d1a070eabd4e935c73bc3ff
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-11-23 15:46:00 +0100

    Program that accesses dictionary key's element by index.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/List/program-65.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env  python3
+
+############################################################################################
+#                                                                                          #
+#       Program purpose: Accesses dictionary key's element by index.                       #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                             #
+#       Creation Date  : November 18, 2019                                                 #
+#                                                                                          #
+############################################################################################
+
+if __name__ == "__main__":
+    num = {'physics': 80, 'math': 90, 'chemistry': 86}
+    print(list(num)[0])
