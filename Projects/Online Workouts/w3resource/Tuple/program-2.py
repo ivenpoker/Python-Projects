c2e3992ac85920b180529c15c4fc04f8bcf989c8
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-12-13 14:08:30 +0100

    Program that creates a tuple with different data types
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Tuple/program-2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env  python3
+
+############################################################################################
+#                                                                                          #
+#       Program purpose: Program that create a tuple with different data types.            #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                             #
+#       Creation Date  : December 13, 2019                                                 #
+#                                                                                          #
+############################################################################################
+
+if __name__ == "__main__":
+
+    new_tuple = ('tuple', False, 3.2, 1, [], dict(), set())
+    print(f'Tuple of different types: {new_tuple}')
