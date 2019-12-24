2d324e5333c274453c5a0270af2af5e45c8d3948
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-12-24 15:22:27 +0100

    Program that creates the difference between two sets.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Sets/prorgam-8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env  python3
+
+############################################################################################
+#                                                                                          #
+#       Program purpose: Creates the difference between two sets.                          #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                             #
+#       Creation Date  : December 24, 2019                                                 #
+#                                                                                          #
+############################################################################################
+
+from random import randint
+
+def random_set_data(low: int, high: int, size: int) -> set:
+    if size < 0:
+        raise ValueError(f"Invalid size ({size}). Must be > 0")
+    return set([randint(low, high) for _ in range(size)])
+
+if __name__ == "__main__":
+    set_A = random_set_data(low=0, high=10, size=15)
+    set_B = random_set_data(low=0, high=10, size=15)
+
+    print(f'Set A: {set_A}')
+    print(f'Set B: {set_B}')
+    print(f'Difference between sets: {set_A - set_B}')
