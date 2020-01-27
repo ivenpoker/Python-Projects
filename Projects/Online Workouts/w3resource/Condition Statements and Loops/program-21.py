d99361c149c131e1084b6e005979bd17bc125cce
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-01-27 17:35:40 +0100

    Program that prints 'L' to the console.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Condition Statements and Loops/program-21.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env  python3
+
+############################################################################################
+#                                                                                          #
+#       Program purpose: Prints letter 'L' to the console.                                 #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                             #
+#       Creation Date  : January 27, 2019                                                  #
+#                                                                                          #
+############################################################################################
+
+def print_letter_L():
+    [print('*') for x in range(8)]
+    print('*' * 7)
+
+if __name__ == '__main__':
+    print_letter_L()
