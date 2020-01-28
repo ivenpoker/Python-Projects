03a54b82e1c61130e353b93da019db2219eca0e7
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-01-28 21:41:28 +0100

    A sample print for this program.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ Projects/Online Workouts/w3resource/Condition Statements and Loops/program-39.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env  python3
+
+############################################################################################
+#                                                                                          #
+#       Program purpose: Sample print for this program.                                    #
+#       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                             #
+#       Creation Date  : January 28, 2019                                                  #
+#                                                                                          #
+############################################################################################
+
+if __name__ == "__main__":
+
+    print('=' * 20, "[CHRISTIAN DON'T BELIEVE IN SUCH]", '=' * 20)
