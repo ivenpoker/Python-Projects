25ecb7f5d3302a56d7aa8b65f30c8cfa771c3667
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-02-04 12:05:16 +0100

    Program that checks if a word or sentence is a pangram.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- Projects/Online Workouts/w3resource/Functions/program-14.py
+++ Projects/Online Workouts/w3resource/Functions/program-14.py
@@ -2,32 +2,34 @@
 
 ############################################################################################
 #                                                                                          #
-#       Program purpose: Accepts a hyphen-separated sequence of words as input from user   #
-#                        and prints the words in a hyphen-separated sequence after sorting #
-#                        them alphabetically.                                              #
+#       Program purpose: Checks if a word or sentence is a pangram.                        #
 #       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                             #
 #       Creation Date  : February 01, 2020                                                 #
 #                                                                                          #
 ############################################################################################
 
-def obtain_user_data(input_str: str) -> str:
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def obtain_user_data(input_mess: str) -> str:
     user_data, valid = '', False
     while not valid:
         try:
-            user_data = input(input_str)
+            user_data = input(input_mess)
             if len(user_data) == 0:
-                raise ValueError("Oops, data needed!")
+                raise ValueError("Oops, data neede")
             valid = True
         except ValueError as ve:
             print(f'[ERROR]: {ve}')
     return user_data
 
-def process_data(some_data: str) -> str:
-    words = [x.strip() for x in some_data.split('-') if len(x.strip()) > 0]
-    words = sorted(words)       # sort the 'words'
-    return '-'.join(words)
+def is_pangram(some_str: str) -> bool:
+    temp_cnt = 0
+    for x in alphabet:
+        # Check if letter occurs at least once in alpahbet
+        if some_str.count(x.lower()) >= 1:
+            temp_cnt += 1
+    return temp_cnt == len(alphabet)
 
 if __name__ == "__main__":
-
-    main_data = obtain_user_data(input_str='Enter some hyphen-separated string: ')
-    print(f'After processing and sorting: {process_data(some_data=main_data)}')
+    main_str = obtain_user_data(input_mess='Enter string data: ')
+    print(f'Is string pangram ?: {is_pangram(some_str=main_str)}')
