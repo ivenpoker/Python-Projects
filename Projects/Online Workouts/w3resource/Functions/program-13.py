87c10936a2846b172afb41276efbf812580ae700
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-02-04 12:04:51 +0100

    Program that computes Pascal's triangle.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- Projects/Online Workouts/w3resource/Functions/program-13.py
+++ Projects/Online Workouts/w3resource/Functions/program-13.py
@@ -2,34 +2,43 @@
 
 ############################################################################################
 #                                                                                          #
-#       Program purpose: Checks if a word or sentence is a pangram.                        #
+#       Program purpose: Computes and prints the Pascal's triangle.                        #
 #       Program Author : Happi Yvan <ivensteinpoker@gmail.com>                             #
 #       Creation Date  : February 01, 2020                                                 #
 #                                                                                          #
 ############################################################################################
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-def obtain_user_data(input_mess: str) -> str:
-    user_data, valid = '', False
+def obtain_triangle_size(input_mess: str) -> int:
+    user_size, valid = int(-1), False
     while not valid:
         try:
-            user_data = input(input_mess)
-            if len(user_data) == 0:
-                raise ValueError("Oops, data neede")
+            user_size = int(input(input_mess))
+            if user_size < 0:
+                raise ValueError("Invalid triangle size. Must be > 0")
             valid = True
         except ValueError as ve:
             print(f'[ERROR]: {ve}')
-    return user_data
+    return user_size
 
-def is_pangram(some_str: str) -> bool:
-    temp_cnt = 0
-    for x in alphabet:
-        # Check if letter occurs at least once in alpahbet
-        if some_str.count(x.lower()) >= 1:
-            temp_cnt += 1
-    return temp_cnt == len(alphabet)
+def print_triangle(main_size: int) -> None:
+    core_data = []
+    curr_row = []
+    for x in range(1, main_size+1):
+        if x == 1 or x == 2:
+            curr_row.append(1)
+            core_data.append(curr_row[:])
+        else:
+            temp, temp_sum = 1, []
+            while temp < len(curr_row):
+                temp_sum.append(curr_row[temp-1] + curr_row[temp])
+                temp += 1
+            curr_row = curr_row[:1] + temp_sum + curr_row[-1:]
+            core_data.append(curr_row[:])
+
+    for row in core_data:
+        print(row)
 
 if __name__ == "__main__":
-    main_str = obtain_user_data(input_mess='Enter string data: ')
-    print(f'Is string pangram ?: {is_pangram(some_str=main_str)}')
+
+    triangle_size = obtain_triangle_size(input_mess='Enter triangle size: ')
+    print_triangle(main_size=triangle_size)
